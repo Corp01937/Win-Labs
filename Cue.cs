593531934aef45b6890753e80268784cbfef1aa2
Corp_i1 <96797729+Corp01937@users.noreply.github.com>
2024-09-08 23:37:01 +0100

    BIgn

---
+++ Cue.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Win_Labs
+{
+    public class Cue : INotifyPropertyChanged
+    {
+        private float cueNumber; // Field for storing CueNumber
+        private string duration;
+        private string preWait;
+        private string postWait;
+        private bool autoFollow;
+        private string fileName;
+        private string targetFile;
+        private string notes;
+
+        public float CueNumber
+        {
+            get => cueNumber;
+            set
+            {
+                if (cueNumber != value)
+                {
+                    cueNumber = value;
+                    OnPropertyChanged(nameof(CueNumber));
+                    Console.WriteLine("PropertyChange.CueNumber");
+                }
+            }
+        }
+
+        private string _CueName;
+        public string CueName
+        {
+            get => _CueName;
+            set
+            {
+                if (_CueName != value)
+                {
+                    _CueName = value;
+                    OnPropertyChanged(nameof(CueName));
+                }
+            }
+        }
+
+
+        public string Duration
+        {
+            get => duration;
+            set
+            {
+                if (duration != value)
+                {
+                    duration = value;
+                    OnPropertyChanged(nameof(Duration));
+                    Console.WriteLine("PropertyChange.Duration");
+                }
+            }
+        }
+
+        public string PreWait
+        {
+            get => preWait;
+            set
+            {
+                if (preWait != value)
+                {
+                    preWait = value;
+                    OnPropertyChanged(nameof(PreWait));
+                    Console.WriteLine("PropertyChange.PreWait");
+                }
+            }
+        }
+
+        public string PostWait
+        {
+            get => postWait;
+            set
+            {
+                if (postWait != value)
+                {
+                    postWait = value;
+                    OnPropertyChanged(nameof(PostWait));
+                    Console.WriteLine("PropertyChange.PostWait");
+                }
+            }
+        }
+
+        public bool AutoFollow
+        {
+            get => autoFollow;
+            set
+            {
+                if (autoFollow != value)
+                {
+                    autoFollow = value;
+                    OnPropertyChanged(nameof(AutoFollow));
+                    Console.WriteLine("PropertyChange.AutoFollow");
+                }
+            }
+        }
+
+        public string FileName
+        {
+            get => fileName;
+            set
+            {
+                if (fileName != value)
+                {
+                    fileName = value;
+                    OnPropertyChanged(nameof(FileName));
+                    Console.WriteLine("PropertyChange.FileName");
+                }
+            }
+        }
+
+        public string TargetFile
+        {
+            get => targetFile;
+            set
+            {
+                if (targetFile != value)
+                {
+                    targetFile = value;
+                    OnPropertyChanged(nameof(TargetFile));
+                    Console.WriteLine("PropertyChange.TargetFile");
+                }
+            }
+        }
+
+        public string Notes
+        {
+            get => notes;
+            set
+            {
+                if (notes != value)
+                {
+                    notes = value;
+                    OnPropertyChanged(nameof(Notes));
+                    Console.WriteLine("PropertyChange.Notes");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            Console.WriteLine("PropertyChange.Detected");
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ StartupWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using Win_Labs;
+
+namespace Win_Labs
+{
+    public partial class StartupWindow : Window
+    {
+        public StartupWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateNewPlaylist_Click(object sender, RoutedEventArgs e)
+        {
+            var folderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            if (folderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string playlistFolderPath = folderDialog.SelectedPath;
+                Console.WriteLine("Selected Path: "+playlistFolderPath);
+                // Open the MainWindow with the selected playlist folder
+                var mainWindow = new MainWindow(playlistFolderPath);
+                mainWindow.Show();
+                this.Close();
+            }
+        }
+
+        private void OpenExistingPlaylist_Click(object sender, RoutedEventArgs e)
+        {
+            var folderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            if (folderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string playlistFolderPath = folderDialog.SelectedPath;
+                Console.WriteLine("Selected Path: " + playlistFolderPath);
+                // Open the MainWindow with the selected playlist folder
+                var mainWindow = new MainWindow(playlistFolderPath);
+                mainWindow.Show();
+                this.Close();
+            }
+        }
+    }
+}
